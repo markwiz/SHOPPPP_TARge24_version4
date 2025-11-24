36e2d93bbd966e462877558309295a7e7d26d4a5
markwiz <mmark.kuusik@gmail.com>
2025-11-24 09:49:07 -0800

    fixx 9329329239239239

---
+++ ShopTARge24/ShopTARge24.KindergartenTest/Testbase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopTARge24.Data;
+
+namespace ShopTARge24.KindergartenTest
+{
+    public abstract class TestBase : IDisposable
+    {
+        protected readonly ServiceProvider Provider;
+        protected readonly ShopTARge24Context DbContext;
+
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+
+            // InMemory versioon ShopTARge24Contextist
+            services.AddDbContext<ShopTARge24Context>(options =>
+                options.UseInMemoryDatabase($"ShopTARge24TestDb_{Guid.NewGuid()}"));
+
+            Provider = services.BuildServiceProvider();
+            DbContext = Provider.GetRequiredService<ShopTARge24Context>();
+
+            DbContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            DbContext.Database.EnsureDeleted();
+            DbContext.Dispose();
+            Provider.Dispose();
+        }
+    }
+}
---
+++ ShopTARge24/ShopTARge24.KindergartenTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopTARge24.KindergartenTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
